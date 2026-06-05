a860a9f6c54e9943ab775aa3d92c1cf26304eb5d
Nghia-Le1707 <nghiahuule101@gmail.com>
2026-06-05 11:37:40 +0700

    fix: database structure with loose type check for strapi v5

--- config/database.ts
+++ config/database.ts
@@ -2,35 +2,33 @@
 /* eslint-disable import/no-anonymous-default-export */
 import path from "path";
 
-export default ({ env }: { env: any }) => {
+export default ({ env }: { env: any }): any => {
   const isProduction = env("NODE_ENV") === "production";
 
-  if (isProduction) {
-    return {
-      client: "postgres",
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: { rejectUnauthorized: false },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-    };
-  }
-
   return {
-    client: "sqlite",
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db"),
-      ),
+      client: isProduction ? "postgres" : "sqlite",
+      connection: isProduction
+        ? {
+            connectionString: env("DATABASE_URL"),
+            ssl: { rejectUnauthorized: false },
+          }
+        : {
+            filename: path.join(
+              __dirname,
+              "..",
+              "..",
+              env("DATABASE_FILENAME", ".tmp/data.db"),
+            ),
+          },
+      useNullAsDefault: !isProduction,
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      pool: isProduction
+        ? {
+            min: env.int("DATABASE_POOL_MIN", 2),
+            max: env.int("DATABASE_POOL_MAX", 10),
+          }
+        : undefined,
     },
-    useNullAsDefault: true,
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
   };
 };
