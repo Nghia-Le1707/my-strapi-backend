2ea242a08954d103159b4b1fb9f6b76758116f01
Nghia-Le1707 <nghiahuule101@gmail.com>
2026-06-05 11:26:57 +0700

    fix: database config structure compatible with strapi v5 production

--- config/database.ts
+++ config/database.ts
@@ -1,41 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable import/no-anonymous-default-export */
 import path from "path";
-import type { Core } from "@strapi/strapi";
 
-const config = ({
-  env,
-}: Core.Config.Shared.ConfigParams): Core.Config.Database => {
+export default ({ env }: { env: any }) => {
   const isProduction = env("NODE_ENV") === "production";
 
-  const dbConfig: any = {
-    connection: {
-      client: isProduction ? "postgres" : "sqlite",
-      connection: isProduction
-        ? {
-            connectionString: env("DATABASE_URL"),
-            ssl: { rejectUnauthorized: false },
-          }
-        : {
-            filename: path.join(
-              __dirname,
-              "..",
-              "..",
-              env("DATABASE_FILENAME", ".tmp/data.db"),
-            ),
-          },
-      useNullAsDefault: !isProduction,
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-    },
-  };
-
   if (isProduction) {
-    dbConfig.connection.pool = {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          connectionString: env("DATABASE_URL"),
+          ssl: { rejectUnauthorized: false },
+        },
+        pool: {
+          min: env.int("DATABASE_POOL_MIN", 2),
+          max: env.int("DATABASE_POOL_MAX", 10),
+        },
+        acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      },
     };
   }
 
-  return dbConfig;
+  return {
+    connection: {
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db"),
+        ),
+      },
+      useNullAsDefault: true,
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
 };
-
-export default config;
